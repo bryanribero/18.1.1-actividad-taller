2d36d5a9fbcb33e64a4e7cfa8f40b3ffc70803f6
Bryan <bryanribero@hotmail.com>
2024-12-01 11:20:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get("/todo/:id", async (req, res) => {
   try {
     conn = await pool.getConnection();
     const rows = await conn.query(
-      "SELECT id, name, description, status FROM todo WHERE id=?",
+      "SELECT id, name, description, created_at, updated_at, status FROM todo WHERE id=?",
       [req.params.id]
     );
 
